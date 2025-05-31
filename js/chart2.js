46691580e0812101ad13b15e8e1e31fb286326cd
XanderM-android <xandermckellarbusiness@gmail.com>
2025-06-01 00:12:32 +1000

    visualisation scripts updated

--- js/chart2.js
+++ js/chart2.js
@@ -1,4 +1,4 @@
-function drawChart1(contId) {
+function drawChart2(contId) {
     var w = 1300; //width. making this one lengthwise
     var h = 700;  //height should probably be smaller due to contents
     var margin = { top: 125, right: 100, bottom: 100, left: 160 }; //jpading. made left a bit bigger to accomodate China's label
@@ -24,33 +24,36 @@ function drawChart1(contId) {
 		.style("pointer-events", "none");
 
 
-    d3.csv("csv/OECD_life_expectancy_1960-2023.csv").then(data => {
-        data.forEach(d => {
+    d3.csv("csv/OECD_years_of_life_lost.csv").then(data => {
+
+        var nuovaData = data.filter(d => d.DEATH_CAUSE === "_T"); //only total cause of death
+
+        nuovaData.forEach(d => {
             d.year = +d.TIME_PERIOD;
-            d.value = +d.VALUE;
+            d.value = +d.OBS_VALUE;
         });
 
-        var contArr = Array.from(new Set(data.map(d => d.COUNTRY))); 
-        var yearArr = Array.from(new Set(data.map(d => d.year))).sort((a, b) => a - b);
+        var contArr = Array.from(new Set(nuovaData.map(d => d["Reference area"])));
+        var yearArr = Array.from(new Set(nuovaData.map(d => d.year))).sort((a, b) => a - b);
 
-        var x = d3.scaleBand().range([0, width]).domain(yearArr).padding(0.05);      //creace scaleband for x axis (years)
-        var y = d3.scaleBand().range([0, height]).domain(contArr).padding(0.05); //ditto, but for y axis (countries)
+        var x = d3.scaleBand().range([0, width]).domain(yearArr).padding(0.05);
+        var y = d3.scaleBand().range([0, height]).domain(contArr).padding(0.05);
 
 		var gradient = d3.scaleLinear()
 			.domain(d3.extent(data, d => d.value))
-			.range(["#E4DFD8", "#442266"]); // colours that work for a kind of monochrome look to account for differnt monitor settings.
+			.range(["#1c2b3b", "#92f79d"]); // colours that work for a kind of monochrome look to account for differnt monitor settings.
 											// could maybe be better but it seems to work fine.
 											// different ones shall be chosen for next vis.
-
+											
 
 	svg.append("g")
 		.selectAll()
-		.data(data)
+		.data(nuovaData)
 		.enter()
-		
+			
 		.append("rect")
 		.attr("x", d => x(d.year))     //set position by year
-		.attr("y", d => y(d.COUNTRY))  //and country
+		.attr("y", d => y(d["Reference area"]))  //and country
 		.attr("width", x.bandwidth())
 		.attr("height", y.bandwidth())
 		.attr("rx", 2) 			//round the corners of each cell
@@ -66,7 +69,7 @@ function drawChart1(contId) {
 				.transition()
 				.duration(100)
 				.attr("x", x(d.year) - 3)
-				.attr("y", y(d.COUNTRY) - 1.5)
+				.attr("y", y(d["Reference area"]) - 1.5)
 				.attr("width", x.bandwidth() + 4) //    increase height
 				.attr("height", y.bandwidth() + 4) ///  same with width
 				.attr("rx", 3) 						//round the edges further
@@ -79,21 +82,21 @@ function drawChart1(contId) {
 				.transition()
 				.duration(100)
 				.attr("x", x(d.year))
-				.attr("y", y(d.COUNTRY))
+				.attr("y", y(d["Reference area"]))
 				.attr("width", x.bandwidth())
 				.attr("height", y.bandwidth())
+				.style("fill", gradient(d.value))
 				.attr("rx", 2) 			//round the corners of each cell
-				.attr("ry", 2) 			//ditto
-				.style("fill", gradient(d.value)); //revert to original colour
-				
-			tooltip.transition().duration(100).style("opacity", 0); //reset tooltip opacity
+				.attr("ry", 2); 			//ditto
+
+			tooltip.transition().duration(100).style("opacity", 0);
 			
-		})
-		
-		
+            })
+
+
 		.on("click", function(event, d) {
 			tooltip.transition().duration(100).style("opacity", 0.8); //set opacity to 0.8. visible, but not distractingly bright
-			tooltip.html("The average life<br>expetcancy (years) for <br>" + d.COUNTRY + " in " + d.year + " was: " + d.value) //pulls data and displays from strings
+			tooltip.html("Total potential years<br>of life lost for<br>" + d["Reference area"] + " in " + d.year + " was: " + d.value) //pulls data and displays from strings
 				.style("left", (event.pageX + 10) + "px")
 				.style("top", (event.pageY - 30) + "px");
 		});
@@ -105,21 +108,21 @@ function drawChart1(contId) {
 
 		svg.append("g")
 			.call(d3.axisLeft(y));
-				
-		svg.append("text") //title
-			.attr("x", 0)
-			.attr("y", -50)
-			.attr("text-anchor", "start")
-			.style("font-size", "25px")
-			.text("Average Life Expectancy by Country Per Year");
 
-		svg.append("text")
-			.attr("x", 0)
-			.attr("y", -26)
-			.attr("text-anchor", "start")
-			.style("font-size", "14px")
-			.text("The averaged life expectancy of multiple countries between 1960 and 2023*, averaged to one decimal point. Click highlighted cell to expand details.");
-			
+        svg.append("text")
+            .attr("x", 0)
+            .attr("y", -50)
+            .attr("text-anchor", "start")
+            .style("font-size", "25px")
+            .text("Potential Years of Life Lost (PYLL) per Country by Year");
+
+        svg.append("text")
+            .attr("x", 0)
+            .attr("y", -26)
+            .attr("text-anchor", "start")
+            .style("font-size", "14px")
+			.text("The total PYLL of multiple countries between 1960 and 2022*. Click highlighted cell to expand details.");
+
 		svg.append("text")
 			.attr("x", 0)
 			.attr("y", -10)
@@ -132,10 +135,10 @@ function drawChart1(contId) {
 		
 		var legendWidth = 550; //width of legend
 		var legendHeight = 8;  //height
-		var legendValues = [30, 40, 50, 60, 70, 80, 90]; //values for legend. intervals of 10
-		var legendAdd = svg.append("g")
+		var legendValues = [10000, 20000, 30000, 40000, 50000]; //values for legend. intervals of 10
+        var legendAdd = svg.append("g")
 			.attr("transform", "translate(0," + (height + 40) + ")");
-			
+
 
 
 		legendAdd.selectAll("rect")
@@ -152,11 +155,11 @@ function drawChart1(contId) {
 			.data(legendValues)
 			.enter()
 			.append("text")
-			.attr("x", (d, i) => i * (legendWidth / legendValues.length) + 35)
+			.attr("x", (d, i) => i * (legendWidth / legendValues.length) + 40)
 			.attr("y", 10)
-			.style("font-size", "12px")
+			.style("font-size", "10px")
 			.text(d => d.toFixed(0));	
-	});
+    });
 		
 		
 		
